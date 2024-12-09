be08e4045c42278d1d3c803061ee774fbf8f6389
mrLuisFer <mrluisfeer@gmail.com>
2024-12-09 12:52:44 -0600

    move static values to dynamic

--- src/app/components/DragAndDrop/index.tsx
+++ src/app/components/DragAndDrop/index.tsx
@@ -4,11 +4,12 @@ import { DndProvider } from "react-dnd";
 import { CustomDragLayer } from "./Custom/CustomDragLayer";
 
 export default function DragAndDrop() {
+  const snapToGrid = false;
   return (
     <DndProvider backend={HTML5Backend}>
       <div className="h-screen w-screen overflow-y-scroll transition hover:bg-neutral-50">
-        <Container snapToGrid={false} />
-        <CustomDragLayer snapToGrid={false} />
+        <Container snapToGrid={snapToGrid} />
+        <CustomDragLayer snapToGrid={snapToGrid} />
       </div>
     </DndProvider>
   );
